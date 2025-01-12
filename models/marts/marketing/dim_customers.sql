b7ad36ad4914c5e248a2073c2155edb95cfa5327
fehmmi aliti <fehmmialiti@gmail.com>
2025-01-12 09:58:17 +0000

    final

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -9,27 +9,26 @@ orders as (
     select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
-
-customer_orders as (
-
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
 payment as (
     select 
     customer_id,
-    sum(amount) as total_amount
+    sum(iff(status = 'success', amount, 0)) as total_amount
     from {{ ref('fct_orders') }}
     group by 1
+),
+customer_orders as (
+
+    select
+        orders.customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(payment.total_amount) as lifetime_value
+    from orders
+        left join payment on orders.customer_id = payment.customer_id
+    group by 1
+    order by 1 asc
+
 ),
 final as (
 
@@ -40,12 +39,10 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
-        total_amount as lifetime_value
+        lifetime_value
 
     from customers
-
     left join customer_orders using (customer_id)
-    left join fct_orders using (customer_id)
 
 )
 
